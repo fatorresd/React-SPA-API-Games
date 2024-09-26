c4db7aa44460f0cbadfd0519b47334452bbb505a
Fatorres <ftorres2018@alu.uct.cl>
2024-09-26 13:26:55 -0300

    Added download button for the JOSN array

---
+++ src/games/functions/downloadJson.js
@@ -0,0 +1,23 @@
+// Objetivo: Función para descargar los juegos en formato JSON
+export const downloadJson = () => {
+    // Función para descargar los juegos en formato JSON
+    const downloadJSON = (gamesArray) => {
+        console.log("Exportando juegos...");
+        const json = JSON.stringify(gamesArray, null, 2);
+        const blob = new Blob([json], { type: "application/json" });
+        const url = URL.createObjectURL(blob);
+            
+        // Crear un enlace para descargar el archivo
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = "games.json"; // Nombre del archivo
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link); // Eliminar el enlace después de la descarga
+        
+        console.log("Juegos exportados correctamente.");
+    };
+  return (
+    { downloadJSON }
+  )
+}
--- src/games/routes/GamesRoutes.jsx
+++ src/games/routes/GamesRoutes.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { Navbar } from '../../ui';
-import { ValvePage, RockstarPage, CdProjectPage, GotyGames, SearchPage, Game} from '../../games';
+import { ValvePage, RockstarPage, CdProjectPage, GotyGames, SearchPage, Game, GamesListConnect} from '../../games';
 
 export const GamesRoutes = () => {
   return (
@@ -9,7 +9,7 @@ export const GamesRoutes = () => {
 
         <div className='container'>
             <Routes>
-                <Route path="valve" element={<ValvePage />} />
+                <Route path="valve" element={<GamesListConnect />} />
                 <Route path="rockstar" element={<RockstarPage />} />
                 <Route path="cdproject" element={<CdProjectPage/>} />
                 <Route path="gotygames" element={<GotyGames/>} />
